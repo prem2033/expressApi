559da5a93ea6f0107ce0802dbb40627a6c065140
Prem Prakash <42450106+prem2033@users.noreply.github.com>
2025-09-09 20:35:28 +0000

    feat: initialize express API with CRUD functionality
    - Added package.json for project dependencies and scripts
    - Created server.js to set up the express server and middleware
    - Implemented basic auth routes (placeholder)
    - Added utility for tracking failed login attempts (placeholder)

---
+++ express-API-CRUD/middleware/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const secret=  process.env.JWT_TOKEN;
+
+export function authenticateToken(req, res, next){
+    // try{
+        const token = res.headers?.['authorization']?.split(" ")[1];
+    if(!token){
+         return res.status(401).json({statsu :'unauthroized' , error : 'Please provide valid token'});
+    }
+    jwt.verify(token, secret, (err, user)=>{
+        if(err){
+            return res.status(403).json({error : 'Invalid Token'});
+        }
+        next();
+    })
+    // }catch(err){
+    //     console.log('Error validating token', {err});
+    //     return res.statsu(500).json({error : 'Unauthorized user'});
+    // }
+
+}
